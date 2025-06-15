f1163653193937be4014c5cbf95ac86394374c98
liuyewen111 <liuywewen111@gmail.com>
2025-06-15 19:17:12 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Blog, Images, Cloud, Voicemail, Link, Play, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Blog, Images, Cloud, MailBulk, Link, PlayCircle, LaptopCode, Sms } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -66,9 +66,10 @@ const siteIcon = {
   Blog,
   Cloud,
   Images,
-  Voicemail,
+  MailBulk,
   Link,
-  Play,
+  PlayCircle,
+  Sms,
   LaptopCode,
 };
 
